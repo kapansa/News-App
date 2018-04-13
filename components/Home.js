15c9575767cf48117b446e70f8393b81f8e02756
kapansa <samuelkapansa@gmail.com>
2018-04-13 15:13:04 +0200

    first commit

---
+++ components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, ImageBackground } from 'react-native';
+
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <ImageBackground source={require('../img/back.jpg')} style={{flex: 1}}>
+          
+      </ImageBackground>
+    );
+  }
+}
+
